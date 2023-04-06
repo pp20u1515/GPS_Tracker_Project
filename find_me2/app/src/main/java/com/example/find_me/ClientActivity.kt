0b2d98c444368df121f7620fe077c7ebbea11512
pp20u1515 <pante.piskunov@gmail.com>
2023-04-06 16:29:32 +0200

    first push

---
+++ find_me2/app/src/main/java/com/example/find_me/ClientActivity.kt
@@ -0,0 +1,8 @@
+package com.example.find_me
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.OnMapReadyCallback
+
+class ClientActivity: AppCompatActivity() {
+
+}
